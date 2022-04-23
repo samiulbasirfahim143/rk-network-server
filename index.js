f55fb829e593e027c1f29b973d2c4716021f1ee9
samiulbasirfahim <samiulbasirfahim@hotmail.com>
2022-04-23 21:30:56 +0600

    package update

--- index.js
+++ index.js
@@ -18,23 +18,39 @@ const run = async () => {
 	await client.connect()
 	const manageDatabase = client.db("Rk-network").collection("package")
 	try {
-        app.post('/package', async(req, res) => {
-            const packageDoc = req.body
-            console.log(req.body);
-            const result = await manageDatabase.insertOne(packageDoc)
-            res.send(result)
-        })
-
+		app.post("/package", async (req, res) => {
+			const packageDoc = req.body
+			console.log(req.body)
+			const result = await manageDatabase.insertOne(packageDoc)
+			res.send(result)
+		})
+		app.put("/package-update/:id", async (req, res) => {
+			const { id } = req.params
+			const data = req.body
+			const query = { _id: ObjectId(id) }
+			const options = { upsert: true }
+			const updateDoc = {
+				$set: {
+					...data,
+				},
+			}
+			const result = await manageDatabase.updateOne(
+				query,
+				updateDoc,
+				options
+			)
+			res.send(result)
+		})
 		app.get("/packages", async (req, res) => {
 			const query = {}
-            const cursor = manageDatabase.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+			const cursor = manageDatabase.find(query)
+			const result = await cursor.toArray()
+			res.send(result)
 		})
 
-		app.delete('/package-del/:id',async (req, res) => {
-			const {id} = req.params
-			const query = {_id: ObjectId(id)}
+		app.delete("/package-del/:id", async (req, res) => {
+			const { id } = req.params
+			const query = { _id: ObjectId(id) }
 			const result = await manageDatabase.deleteOne(query)
 			res.send(result)
 		})
@@ -45,4 +61,6 @@ const run = async () => {
 
 run().catch(console.dir)
 
-app.listen(process.env.PORT || port, () => console.log("listening to port" + port))  
+app.listen(process.env.PORT || port, () =>
+	console.log("listening to port" + port)
+)
